86c2e19fc1734ed78b0e1727cf17a327b6518741
kejilion <131984541+kejilion@users.noreply.github.com>
2023-05-03 18:29:07 +0800

    Add files via upload
    配置多个VPS信息，配置已经解析到服务器上的域名，配置重定向到的网站
    VPS先安装docker环境！
    随后一键运行脚本，将自动申请证书，配置NGINX，安装启动NGINX，搞定！

---
+++ 一键所有VPS申请域名证书部署NGINX重定向.py
@@ -0,0 +1,148 @@
+import paramiko
+
+# 定义服务器列表，包括服务器名称、IP地址、端口号、用户名、密码和解析好的域名
+servers = [
+
+    {"name": "美国", "hostname": "1.1.1.1", "port": 22, "username": "root", "password": "123456", "domain": "yuming.com"},   
+    {"name": "不丹", "hostname": "1.1.1.1", "port": 22, "username": "root", "password": "123456", "domain": "yuming.com"},   
+    {"name": "毛里求斯", "hostname": "1.1.1.1", "port": 22, "username": "root", "password": "123456", "domain": "yuming.com"},   
+    # 添加更多服务器
+
+]
+
+
+
+# 定义更新操作
+def update_server(name, hostname, port, username, password, domain):
+    try:
+
+        # 连接服务器
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname, port=port, username=username, password=password)
+
+
+        print(f" {name} 更新")
+        stdin, stdout, stderr = client.exec_command("apt update -y && apt install -y curl wget sudo socat")
+        
+        print(f"正在更新:")
+        while not stdout.channel.exit_status_ready():
+            if stdout.channel.recv_ready():
+                print(stdout.channel.recv(1024).decode(), end="")
+
+        # 检查执行状态
+        if stderr.channel.recv_exit_status() == 0:
+            print(f"更新成功")
+        else:
+            print(f"更新失败")
+        
+        print()
+
+
+        print(f"{name} 创建nginx目录")
+        stdin, stdout, stderr = client.exec_command("mkdir -p /home/nginx\ntouch /home/nginx/nginx.conf\nmkdir -p /home/nginx/certs")
+
+        while not stdout.channel.exit_status_ready():
+            if stdout.channel.recv_ready():
+                print(stdout.channel.recv(1024).decode(), end="")
+
+        # 检查执行状态
+        if stderr.channel.recv_exit_status() == 0:
+            print(f"创建目录成功")
+        else:
+            print(f"创建目录失败")
+
+        print()
+
+        print(f"{name} 申请证书")
+        #stdin, stdout, stderr = client.exec_command("curl https://get.acme.sh | sh && ~/.acme.sh/acme.sh --register-account -m xxxx@gmail.com && ~/.acme.sh/acme.sh --issue -d kjlion.gq --standalone")
+        stdin, stdout, stderr = client.exec_command("curl https://get.acme.sh | sh && ~/.acme.sh/acme.sh --register-account -m xxxx@gmail.com && ~/.acme.sh/acme.sh --issue -d {} --standalone".format(domain))
+        print(f"正在申请中:")
+        while not stdout.channel.exit_status_ready():
+            if stdout.channel.recv_ready():
+                print(stdout.channel.recv(1024).decode(), end="")
+
+        # 检查执行状态
+        if stderr.channel.recv_exit_status() == 0:
+            print(f"申请成功")
+        else:
+            print(f"申请失败")
+
+        print()
+
+        print(f"{name} 下载证书")
+        #stdin, stdout, stderr = client.exec_command("~/.acme.sh/acme.sh --installcert -d kjlion.gq --key-file /home/nginx/certs/key.pem --fullchain-file /home/nginx/certs/cert.pem")
+        stdin, stdout, stderr = client.exec_command("~/.acme.sh/acme.sh --installcert -d {} --key-file /home/nginx/certs/key.pem --fullchain-file /home/nginx/certs/cert.pem".format(domain))
+        while not stdout.channel.exit_status_ready():
+            if stdout.channel.recv_ready():
+                print(stdout.channel.recv(1024).decode(), end="")
+
+        # 检查执行状态
+        if stderr.channel.recv_exit_status() == 0:
+            print(f"下载证书成功")
+        else:
+            print(f"下载证书失败")
+
+        print()        
+
+        print(f"{name} 配置nginx")
+
+        # 重定向到那个网站
+        redirect = "www.bilibili.com"
+        stdin, stdout, stderr = client.exec_command('wget -O /home/nginx/nginx.conf https://raw.githubusercontent.com/kejilion/nginx/main/nginx.conf && sed -i "s/yuming.com/' + domain + '/g" /home/nginx/nginx.conf && sed -i "s/www.baidu.com/' + redirect + '/g" /home/nginx/nginx.conf')
+        while not stdout.channel.exit_status_ready():
+            if stdout.channel.recv_ready():
+                print(stdout.channel.recv(1024).decode(), end="")
+
+        # 检查执行状态
+        if stderr.channel.recv_exit_status() == 0:
+            print(f"配置成功")
+        else:
+            print(f"配置失败")
+
+        print()
+
+        print(f"{name} 启动nginx")
+        stdin, stdout, stderr = client.exec_command('docker run -d --name nginx --restart=always -p 80:80 -p 443:443 -v /home/nginx/nginx.conf:/etc/nginx/nginx.conf -v /home/nginx/certs:/etc/nginx/certs -v /home/nginx/html:/usr/share/nginx/html nginx:latest')
+        print(f"启动中:")
+        while not stdout.channel.exit_status_ready():
+            if stdout.channel.recv_ready():
+                print(stdout.channel.recv(1024).decode(), end="")
+
+        # 检查执行状态
+        if stderr.channel.recv_exit_status() == 0:
+            print(f"启动成功")
+        else:
+            print(f"启动失败")
+
+        print()
+
+        print(f"{name} nginx启动状态")
+        stdin, stdout, stderr = client.exec_command('docker ps -a')
+        while not stdout.channel.exit_status_ready():
+            if stdout.channel.recv_ready():
+                print(stdout.channel.recv(1024).decode(), end="")
+
+        print()
+        print()
+
+        # 关闭 SSH 连接
+        client.close()
+
+
+    except Exception as e:
+        print(f"连接 {name} 失败")
+
+
+# 遍历服务器列表，逐一更新
+for server in servers:
+    name = server["name"]
+    hostname = server["hostname"]
+    port = server["port"]
+    username = server["username"]
+    password = server["password"]
+    domain = server["domain"]
+    update_server(name, hostname, port, username, password, domain)
+
+# 等待用户按下任意键后关闭窗口
+input("按任意键关闭窗口...")
